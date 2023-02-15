ecc8833f011d7df091f36c232830a34079a281ba
Alina <alinatussupova@gmail.com>
2023-02-15 10:06:36 +0600

    Initial Commit.

--- checkmypass.py
+++ checkmypass.py
@@ -2,12 +2,15 @@ import requests
 import hashlib
 import sys
 
+
+# Requests our data and gives us a response. "query_char" == hashed version of our data. 
 def request_api_data(query_char):
     url = "https://api.pwnedpasswords.com/range/" + query_char
     res = requests.get(url)
     if res.status_code != 200:
-        raise RuntimeError(f"Error fetching: {res.status_code}, check the API and try again")
+        raise RuntimeError(f"Error fetching: {res.status_code}, check the API and try again.")
     return res
+
 
 def get_password_leaks_count(hashes, hash_to_check):
     hashes = (line.split(":") for line in hashes.text.splitlines())
@@ -16,11 +19,14 @@ def get_password_leaks_count(hashes, hash_to_check):
             return count
     return 0
 
+
+# Check if the password exists in API response
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
     return get_password_leaks_count(response, tail)
+
 
 def main(args):
     for password in args:
@@ -30,6 +36,7 @@ def main(args):
         else:
             print(f'{password} was not found. Your password is strong.')
     return "Done!"
+
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
