8526f4b813158609a9feb48bec03e0801cd08927
Alina <alinatussupova@gmail.com>
2023-02-15 13:30:43 +0600

    Initial Commit.

--- checkmypass.py
+++ checkmypass.py
@@ -12,29 +12,34 @@ def request_api_data(query_char):
     return res
 
 
-def get_password_leaks_count(hashes, hash_to_check):
-    hashes = (line.split(":") for line in hashes.text.splitlines())
-    for h, count in hashes:
-        if h == hash_to_check:
+# How many times a password has been leacked
+def get_password_leaks_count(hashes, hash_to_check):   # hash_to_check is the tail end of the hashpassword
+    hashes = (line.split(":") for line in hashes.text.splitlines())     
+    for hash, count in hashes:
+        if hash == hash_to_check:    # check this because API returns a list of the tailed hashes
             return count
     return 0
 
 
 # Check if the password exists in API response
 def pwned_api_check(password):
+    # Converts our password to sha1
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    first5_char, tail = sha1password[:5], sha1password[5:]
+    first5_char, tail = sha1password[:5], sha1password[5:]   # We take only first 5 characters and the remaining.
+    # Call request_api_data()
     response = request_api_data(first5_char)
     return get_password_leaks_count(response, tail)
 
 
+# Receieves arguments we are giving in our cmd
 def main(args):
     for password in args:
+        # receieve counts from get_password_leaks_count()
         count = pwned_api_check(password)
         if count:
             print(f'{password} was found {count} times. Probably you should change your password.')
         else:
-            print(f'{password} was not found. Your password is strong.')
+            print(f'{password} was not found. Your password is strong!')
     return "Done!"
 
 
